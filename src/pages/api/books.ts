f77f407158b08d2956326d3c448a4db2dc5ee72d
sandruumg <sandra.munoz.garcia99@gmail.com>
2024-04-05 17:02:11 +0200

    array libros

--- src/pages/api/books.ts
+++ src/pages/api/books.ts
@@ -1,9 +1,12 @@
 import type { APIRoute } from 'astro';
-
+const libros = [
+  { id: 1, nombre: "El señor de los anillos", titulo: "El señor de los anillos" },
+  { id: 2, nombre: "Harry Potter", titulo: "Harry Potter y la piedra filosofal" },
+  { id: 3, nombre: "Canción de Hielo y Fuego", titulo: "Juego de tronos" }
+];
 export const GET: APIRoute = async ({ params, request }) => {
     return new Response(
-        JSON.stringify(  { id: 1, nombre: "El señor de los anillos", titulo: "El señor de los anillos", autor: "J.R.R. Tolkien", imagen: "/img/71oVTrHAylL._AC_UF894,1000_QL80_.jpg" },
-        ), {
+        JSON.stringify(libros), {
           status: 200,
           headers: {
             "Content-Type": "application/json"
