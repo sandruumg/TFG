aa75d9608ca0dbc662d29a9770edd47dde2aa89c
laura hernandez <laura.hernandez.r@hotmail.com>
2024-04-10 12:35:04 +0200

    Instalacion y configuracion tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import db from "@astrojs/db";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()],
+  integrations: [db(), tailwind()],
   output: "server",
   adapter: vercel()
 });
