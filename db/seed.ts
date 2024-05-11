b6f0ae93b0a5483d0af5b4c5b874d9555a19dac3
sandruumg <sandra.munoz.garcia99@gmail.com>
2024-05-11 16:56:31 +0200

    seccion rankingLectores back, mostrar valoracion infoLibro, seccion rankingLibros a medias

--- db/seed.ts
+++ db/seed.ts
@@ -988,7 +988,7 @@ export default async function seed() {
             sinopsisLibro: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
             nPaginasLibro: 528,
             categoriaLibro: "Terror",
-            fechaPubLibro: new Date("2024-05-23") // date no sé si es correcto	
+            fechaPubLibro: new Date("2023-05-23") // date no sé si es correcto	
         },
         {
             idLibro: 89,
--- src/pages/api/informacionLibro/[idLibro].ts
+++ src/pages/api/informacionLibro/[idLibro].ts
@@ -1,15 +1,33 @@
-import { db, Libros, eq} from 'astro:db';
+import { db, Libros, eq, RankingLibros, sum, count} from 'astro:db';
 
 export const GET = async ({ params }:{params:{idLibro:number}}) => {
     
     let libroEncontrado;
+    let libroValorado;
     if(!params){
         return new Response(null, {
             status: 404,
             statusText: 'Not found'
         });
     }else{
-        libroEncontrado = await db.select().from(Libros).where(eq(Libros.idLibro, params.idLibro));
+        libroValorado = await db.select().from(RankingLibros).where(eq(RankingLibros.idLibro, params.idLibro))
+        if(libroValorado.length > 0){
+          libroEncontrado = await db.select({ cantidadEstrellas: sum(RankingLibros.estrellas),
+                                              cantidadValoraciones: count(RankingLibros.idLibro),
+                                              tituloLibro: Libros.tituloLibro,
+                                              nombreAutor: Libros.nombreAutor,
+                                              portadaLibro: Libros.portadaLibro,
+                                              fondoLibro: Libros.fondoLibro,
+                                              sinopsisLibro: Libros.sinopsisLibro,
+                                              nPaginasLibro: Libros.nPaginasLibro,
+                                              categoriaLibro: Libros.categoriaLibro,
+                                              fechaPubLibro: Libros.fechaPubLibro})
+                                    .from(Libros)
+                                    .innerJoin(RankingLibros, eq(RankingLibros.idLibro, Libros.idLibro))
+                                    .where(eq(Libros.idLibro, params.idLibro));
+        }else {
+          libroEncontrado = await db.select().from(Libros).where(eq(Libros.idLibro, params.idLibro));
+        }
     }
     console.log(libroEncontrado);
     return new Response(
