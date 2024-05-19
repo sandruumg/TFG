847e23f9ac53a2bedd89e3f2fb89338f6cf9874c
sandruumg <sandra.munoz.garcia99@gmail.com>
2024-05-19 18:03:23 +0200

    cambio db

--- db/config.ts
+++ db/config.ts
@@ -8,7 +8,6 @@ const Usuarios = defineTable({
     nombreUsuario: column.text(),    
     apellidoUsuario: column.text(),
     correoUsuario: column.text({unique: true}),
-    generoUsuario: column.text(),
     password: column.text()
   }
 });
