422aba4508919cb5c07bd96c4957c36a5cac938e
sandruumg <sandra.munoz.garcia99@gmail.com>
2025-06-26 16:07:32 +0200

    cambio en nombre token

--- src/turso.ts
+++ src/turso.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client/web";
 
 export const turso = createClient({
-  url: import.meta.env.TURSO_DATABASE_URL,
-  authToken: import.meta.env.TURSO_AUTH_TOKEN,
+  url: import.meta.env.ASTRO_DB_REMOTE_URL,
+  authToken: import.meta.env.ASTRO_DB_APP_TOKEN,
 });
